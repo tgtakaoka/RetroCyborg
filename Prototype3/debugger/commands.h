bbb62a6dfae5d9f1fb5fec9d3ad8b24eead2b12f
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2019-06-13 01:01:02 +0900

    Import debugger to Prototyp3

---
+++ Prototype3/debugger/commands.h
@@ -0,0 +1,18 @@
+#ifndef __commands_h__
+#define __commands_h__
+
+class Commands {
+
+public:
+  void loop();
+  void exec(char c);
+
+private:
+  enum {
+    HALT, STEP, RUN
+  } _target = HALT;
+};
+
+extern Commands Commands;
+
+#endif
