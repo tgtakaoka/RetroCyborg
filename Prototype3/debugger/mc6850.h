fd0b3db07e59ccc98fc68dedc86e377bc7d8844b
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2019-11-26 17:44:53 +0900

    Add ACIA TX irq

--- Prototype3/debugger/mc6850.h
+++ Prototype3/debugger/mc6850.h
@@ -17,45 +17,50 @@ class Mc6850 {
 
   private:
     const uint16_t _baseAddr;
-    const uint8_t _rxIrq;
-    const uint8_t _txIrq;
+    const uint8_t _rxInt;
+    const uint8_t _txInt;
     uint8_t _control;
     uint8_t _status;
     uint8_t _readFlags;
     uint8_t _nextFlags;
-    uint8_t _sendingData;
-    uint8_t _receivedData;
+    uint8_t _txData;
+    uint8_t _rxData;
 
     // Bit Definition of control register
-    // CDSx, WSBx, TCB=3 are ignored.
-    static constexpr uint8_t CDS0 = 0x01; // Counter Divider Select bit 0
-    static constexpr uint8_t CDS1 = 0x02; // Counter Divider Select bit 1
-    static constexpr uint8_t WSB0 = 0x04; // Word Select bit 0
-    static constexpr uint8_t WSB1 = 0x08; // Word Select bit 1
-    static constexpr uint8_t WSB2 = 0x10; // Word Select bit 2
-    static constexpr uint8_t TCB0 = 0x20; // Transmit Control bit 0
-    static constexpr uint8_t TCB1 = 0x40; // Transmit Control bit 1
-    static constexpr uint8_t RIEB = 0x80; // Receive Interrupt Enable bit
+    static constexpr uint8_t CDS_gm       = 0x03; // Counter Divider Select group mask
+    static constexpr uint8_t CDS_DIV1_gc  = 0x00; // Counter Divider /1
+    static constexpr uint8_t CDS_DIV16_gc = 0x01; // Counter Divider /16
+    static constexpr uint8_t CDS_DIV64_gc = 0x02; // Counter Divider /64
+    static constexpr uint8_t CDS_RESET_gc = 0x03; // Master Reset
+    static constexpr uint8_t TCB_gm       = 0x60; // Transmit Control group mask
+    static constexpr uint8_t TCB_DI_gc    = 0x00; // RTS=Low,  Tx Interrupt Disabled
+    static constexpr uint8_t TCB_EI_gc    = 0x20; // RTS=LOW,  Tx Interrupt Enabled
+    static constexpr uint8_t TCB_RTS_gc   = 0x40; // RTS=High, Tx Interrupt Disabled
+    static constexpr uint8_t TCB_BREAK_gc = 0x60; // RTS=Low,  Tx Interrupt Disabled
+                                                // Transmit Break Level
+    static constexpr uint8_t RIEB_bm = 0x80; // Receive Interrupt Enable bit mask
 
-    bool txIntEnabled() const {
-      return (_control & (TCB0 | TCB1)) == TCB0;
-    }
-    bool rxIntEnabled() const {
-      return (_control & RIEB) != 0;
-    }
-    void assertIrq(uint8_t irq);
-    void negateIrq(uint8_t irq);
+    static uint8_t cds(uint8_t control) { return control & CDS_gm; }
+    static uint8_t tcb(uint8_t control) { return control & TCB_gm; }
+    static uint8_t rieb(uint8_t control) { return control & RIEB_bm; }
+    bool masterReset() const { return cds(_control) == CDS_RESET_gc; }
+    bool txIntEnabled() const { return tcb(_control) == TCB_EI_gc; }
+    uint8_t rxIntEnabled() const { return rieb(_control); }
+    uint8_t txRegEmpty() const { return _status & TDRE_bm; }
+    uint8_t rxRegFull() const { return _status & RDRF_bm; }
+    void assertIrq(uint8_t intMask);
+    void negateIrq(uint8_t intMask);
 
     // Bit definition of status register
     // DCD, CTS, FERR, PERR are always zero.
-    static constexpr uint8_t RDRF = 0x01; // Receive Data Register Full
-    static constexpr uint8_t TDRE = 0x02; // Transmit Data Register Empty
-    static constexpr uint8_t DCD  = 0x04; // Data Carieer Detect
-    static constexpr uint8_t CTS  = 0x08; // Clear to Send
-    static constexpr uint8_t FERR = 0x10; // Framing Error
-    static constexpr uint8_t OVRN = 0x20; // Receiver Overrun
-    static constexpr uint8_t PEER = 0x40; // Parity Error
-    static constexpr uint8_t IRQF = 0x80; // Interrupt Request
+    static constexpr uint8_t RDRF_bm = 0x01; // Receive Data Register Full
+    static constexpr uint8_t TDRE_bm = 0x02; // Transmit Data Register Empty
+    static constexpr uint8_t DCD_bm  = 0x04; // Data Carieer Detect
+    static constexpr uint8_t CTS_bm  = 0x08; // Clear to Send
+    static constexpr uint8_t FERR_bm = 0x10; // Framing Error
+    static constexpr uint8_t OVRN_bm = 0x20; // Receiver Overrun
+    static constexpr uint8_t PEER_bm = 0x40; // Parity Error
+    static constexpr uint8_t IRQF_bm = 0x80; // Interrupt Request
 };
 
 #endif
--- Prototype3/samples/echo.asm
+++ Prototype3/samples/echo.asm
@@ -3,72 +3,41 @@
 	.lf	echo.lst
 	.tf	echo.s19,s19
 
-;;; MC6850
-;;; Asynchronous Communication Interface Adapter
-ACIA:	.equ	$FFC0
-	;; Control register
-ACIA_C:	.equ	ACIA+0
-	;; Counter Divider Select Bits
-CDS_0:	.equ	0x00	  ; /1
-CDS_1:	.equ	0x01	  ; /16
-CDS_2:	.equ	0x02	  ; /64
-CDS_3:	.equ	0x03	  ; Master Reset
-	;; Word Select Bits
-WSB_0:	.equ	0x00	  ; 7 Bits + Even Parity + 2 Stop Bits
-WSB_1:	.equ	0x04      ; 7 bits + Odd Parity  + 2 Stop Bits
-WSB_2:	.equ	0x08	  ; 7 bits + Even Parity + 1 Stop Bits
-WSB_3:	.equ	0x0C	  ; 7 bits + Odd Parity  + 1 Stop Bits
-WSB_4:	.equ	0x10	  ; 8 bits + No Parity   + 2 Stop Bits
-WSB_5:	.equ	0x14	  ; 8 bits + No Parity   + 1 Stop Bits
-WSB_6:	.equ	0x18	  ; 8 bits + Even Parity + 1 Stop Bits
-WSB_7:	.equ	0x1C	  ; 8 bits + Odd Parity  + 1 Stop Bits
-	;; Transmit Control Bits
-TCB_0:	.equ	0x00		; RTS=Low,  Interrupt Disabled
-TCB_1:	.equ	0x20		; RTS=Low,  Interrupt Enabled
-TCB_2:	.equ	0x40		; RTS=High, Interrupt Disabled
-TCB_3:	.equ	0x60		; RTS=Low,  Interrupt Disabled
-				; Transmit Break Level
-RIEB:	.equ	0x80		; Receive Interrupt Enable Bit.
-	;; Status register
-ACIA_S:	.equ	ACIA+0
-RDRF:	.equ	0x01		; Receive Data Register Full
-TDRE:	.equ	0x02		; Transmit Data Register Empty
-DCDF:	.equ	0x04		; Data Carrier Detect Flag
-CTSF:	.equ	0x08		; Clear To Send Flag
-FERR:	.equ	0x10		; Frame Error Flag
-OVRN:	.equ	0x20		; Receiver Overrun Flag
-PERR:	.equ	0x40		; Parity Error Flag
-IRQF:	.equ	0x80		; Interrupt Request Flag
-	;; Data register
-ACIA_D:	.equ	ACIA+1		; Data register
+;;; MC6850 Asynchronous Communication Interface Adapter
+ACIA:   .equ	$FFC0
+        .inc    mc6850.inc
 
-        .sm     ram
-        .org    $F000
-stack:  .equ    *
+	.sm	ram
+	.org	$F000
+stack:	.equ	*
 
-        .sm     code
+	.sm	code
 	.org	$1000
 initialize:
 	lds	#stack
-        ;; 8 bits + No Parity + 1 Stop Bits
-        ;; Transmit, Receive interrupts enable
-	lda	#CDS_0|WSB_5|TCB_1|RIEB
-	sta	ACIA_C
-        sei                     ; Set Interrupt mask
+	lda	#CDS_RESET_gc   ; Master reset
+	sta	ACIA_control
+	lda	#WSB_8N1_gc        ; 8 bits + No Parity + 1 Stop Bits
+	ora	#TCB_EI_gc|RIEB_bm ; Transmit, Receive interrupts enable
+	sta	ACIA_control
+	sei			; Set Interrupt mask
 
 receive_loop:
-	lda	ACIA_S
-	bita	#RDRF
+	lda	ACIA_status
+	bita	#RDRF_bm
 	beq	receive_loop
 receive_data:
-	ldb	ACIA_D
+	ldb	ACIA_data
 transmit_loop:
-	lda	ACIA_S
-	bita	#TDRE
+	lda	ACIA_status
+	bita	#TDRE_bm
 	beq	transmit_loop
 transmit_data:
-	stb	ACIA_D
-	bra	receive_loop
+	stb	ACIA_data
+        cmpb    #$0d
+        bne     receive_loop
+        ldb     #$0a
+	bra	transmit_loop
 
 	.no	$FFFE
 	.dw	initialize
