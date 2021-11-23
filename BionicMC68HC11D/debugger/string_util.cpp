5d1e8f4968a36881f3e48cae80bccef5edb64a2e
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2021-11-23 15:59:02 +0900

    [BionicMC68HC11D] Copy debugger from BionicMC6801

---
+++ BionicMC68HC11D/debugger/string_util.cpp
@@ -0,0 +1,23 @@
+#include "string_util.h"
+
+static char toHex4(uint8_t val) {
+    val &= 0xf;
+    return val < 10 ? val + '0' : val - 10 + 'A';
+}
+
+void outHex8(char *p, uint8_t val) {
+    p[0] = toHex4(val >> 4);
+    p[1] = toHex4(val);
+}
+
+void outHex16(char *p, uint16_t val) {
+    outHex8(p + 0, static_cast<uint8_t>(val >> 8));
+    outHex8(p + 2, static_cast<uint8_t>(val));
+}
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// tab-width: 4
+// End:
+// vim: set ft=cpp et ts=4 sw=4:
---
+++ BionicMC68HC11D/debugger/string_util.h
@@ -0,0 +1,16 @@
+#ifndef __STRING_UTIL_H__
+#define __STRING_UTIL_H__
+
+#include <stdint.h>
+
+void outHex8(char *p, uint8_t value);
+void outHex16(char *p, uint16_t value);
+
+#endif
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// tab-width: 4
+// End:
+// vim: set ft=cpp et ts=4 sw=4:
